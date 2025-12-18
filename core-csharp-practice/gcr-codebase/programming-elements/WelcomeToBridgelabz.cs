fed84bd4af7322dc5089d2a7bcbb0df287c72c9e
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-18 16:40:50 +0530

    [Harsh Upadhyay].Add Solution of Welcome To Bridgelabz in C Sharp Language Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/programming-elements/WelcomeToBridgelabz.cs
@@ -0,0 +1,8 @@
+using System;
+namespace package{
+    internal class WelcomeToBridgelabz{
+        public static void main(string[]args){
+            Console.WriteLine("Welcome To Bridgelabz");
+        }
+    }
+}
