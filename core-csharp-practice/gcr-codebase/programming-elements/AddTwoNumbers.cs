8d8182f2650e3abccb90a6225dfde561784594b1
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-18 16:08:31 +0530

    [Harsh Upadhyay].Add Solution of Add Two Numbers in C Sharp Language Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/programming-elements/AddTwoNumbers.cs
@@ -0,0 +1,12 @@
+using System;
+namespace package{
+    internal class AddTwoNumbers{
+        public static void Main(string[]args){
+            int a=2;
+            int b=3;
+            int sum=a+b;
+            Console.WriteLine(sum);
+        }
+    }
+}
+
