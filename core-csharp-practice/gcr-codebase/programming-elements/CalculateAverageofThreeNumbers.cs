763d4aabed2a0e35ab9c44fe3bd95904b2a8259d
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-18 16:41:52 +0530

    [Harsh Upadhyay].Add Solution of Welcome To Calculate Average of Three Numbersin C Sharp Language Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/programming-elements/CalculateAverageofThreeNumbers.cs
@@ -0,0 +1,12 @@
+using System;
+namespace package{
+    internal class CalculateAverageofThreeNumber{
+        public static void Main(string[]args){
+            int firstNumber=15;
+            int secondNumber=16;
+            int thirdNumber=18;
+            double Average=(firstNumber+secondNumber+thirdNumber)/3;
+            Console.WriteLine(Average);
+        }
+    }
+}
