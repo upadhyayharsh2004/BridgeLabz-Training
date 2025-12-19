0d6222aac49d6d9fcc3951e1901d54c567585b5c
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-19 22:25:18 +0530

    [Harsh Upadhyay].Add Practice Problem of Sam's Average Marks and Exe File Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/c#-programming-elements/level1-c#-programming-elements/SamAverageMarks.cs
@@ -0,0 +1,11 @@
+using System;
+class SamAverageMarks{
+    public static void Main(string[] args){
+        int maths=94;
+        int physics=95;
+        int chemistry=96;
+        double averagePercentMarks=((maths+physics+chemistry)/300.0)*100;
+        Console.WriteLine("Sam’s average mark in PCM is"+" "+averagePercentMarks);
+    }
+}
+        
