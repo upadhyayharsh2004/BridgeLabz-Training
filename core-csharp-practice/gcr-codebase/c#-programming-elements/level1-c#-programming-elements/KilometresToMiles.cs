8c49ce185f8f0c79eeebb0c9cd32bb4499297beb
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-19 22:24:12 +0530

    [Harsh Upadhyay].Add Practice Problem of Kilometres To Miles and Exe File Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/c#-programming-elements/level1-c#-programming-elements/KilometresToMiles.cs
@@ -0,0 +1,9 @@
+using System;
+class KilometresToMiles{
+    public static void Main(string[] args){
+        double distanceFromKilometresToMiles=10.8;
+        double distanceInMiles=1.6*distanceFromKilometresToMiles;
+        Console.WriteLine("The distance"+" "+distanceFromKilometresToMiles+" "+"km in miles is"+" "+distanceInMiles);
+    }
+}
+        
