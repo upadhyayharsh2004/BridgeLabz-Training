9b9cc03fa9dc258e51ce1fe6571df03f6e21c925
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-19 23:34:25 +0530

    [Harsh Upadhyay].Add Practice Problem of Total Income File Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/c#-programming-elements/level2-c#-programming-elements/TotalIncome.cs
@@ -0,0 +1,12 @@
+using System;
+class TotalIncome{
+    public static void Main(string[] args){
+        double salary;
+        double bonus;
+        salary=Convert.ToDouble(Console.ReadLine());
+        bonus=Convert.ToDouble(Console.ReadLine());
+        double totalIncome = salary + bonus;
+        Console.WriteLine(
+            "The salary is INR"+" "+"salary and bonus is INR"+" "+bonus+" "+"."+" "+"Hence Total Income is INR"+" "+totalIncome);
+    }
+}
