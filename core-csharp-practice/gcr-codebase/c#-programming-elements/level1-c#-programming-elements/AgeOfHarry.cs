72cf10eb6bc4d9f9505b2daa6db5d8a65189bd75
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-19 21:58:53 +0530

    [Harsh Upadhyay].Add Practice Problems Solution Of Age Of Harry and Exe File Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/c#-programming-elements/level1-c#-programming-elements/AgeOfHarry.cs
@@ -0,0 +1,10 @@
+using System;
+class AgeOfHarry{
+    public static void Main(string[] args){
+        int birthYear=2000;
+        int currentYear=2024;
+        int currentAge=currentYear-birthYear;
+        Console.WriteLine("Harry's age in 2024 is"+" "+currentAge);
+    }
+}
+        
