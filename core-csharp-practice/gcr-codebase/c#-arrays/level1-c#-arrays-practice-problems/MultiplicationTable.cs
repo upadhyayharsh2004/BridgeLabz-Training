c47b98ed56e137c702bc339c978dd913b000866b
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-24 15:01:10 +0530

    [Harsh Upadhyay].Add Practice Problems and Solution Of Multiplication Table Problem Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/c#-arrays/level1-c#-arrays-practice-problems/MultiplicationTable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.arrays
+{
+   internal class MultiplicationTable
+   {
+       public static void Main(string[] args)
+       {
+           //Get an integer input and store it in the number variable.
+           int number = Convert.ToInt32(Console.ReadLine());
+           //Declaring an array with the size of 10 to store multiplication table values
+           int[] multi = new int[10];
+           //Using for loop to iterate through the array to store multiplication table values
+           for (int i = 1; i < 10; i++)
+           {
+               //Storing multiplication table values in the array
+               multi[i] = number * i;
+               //Printing the multiplication of a number in form of a table
+               Console.WriteLine(number + "*" + i + "=" + multi[i]);
+           }
+       }
+   }
+}
