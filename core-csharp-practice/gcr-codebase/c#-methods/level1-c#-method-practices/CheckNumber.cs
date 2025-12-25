8842e181b12e81b27c8d314b1717f95a1b68ea68
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-25 19:19:27 +0530

    [Harsh Upadhyay].Add Practice Problems and Solution Of Check Number Problem Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/c#-methods/level1-c#-method-practices/CheckNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.level1_c__methods_practices
+{
+   internal class CheckNumber
+   {
+       public static void Main(string[] args)
+       {
+           //Getting integer input from the user for checking the number is positive , negative or zero
+           int numberToCheck= Convert.ToInt32(Console.ReadLine());
+           //Creating an instance or object of the class to access non static method in a static method
+           CheckNumber nC = new CheckNumber();
+           //Calling a method to check that the number is positive negative or zero and storing it in a variable to print further
+           string resultOfNumberChecked = nC.NumberChecking(numberToCheck);
+           //Printing that the number is positive negative or zero to the output screen
+           Console.WriteLine("The Number Which Has Been Checked It's" + " " +resultOfNumberChecked);
+       }
+       public string NumberChecking(int numberToChecking)
+       {
+           //Using if else condition to check whether the number is positive negative or zero according to condition and also returning to the method called in a main method
+           if (numberToChecking < 0)
+           {
+               return "A Negative Number";
+           }
+           else if (numberToChecking > 0)
+           {
+               return "A Positive Number";
+           }
+           else
+           {
+               return "A Zero";
+           }
+       }
+   }
+}
