12a728ce11b51fdf899391cdc4cd66e465f22fe1
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-25 19:33:54 +0530

    [Harsh Upadhyay].Add Practice Problems and Solution Of Age Of Vote Checking Problem Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/c#-methods/level2-c#-method-practices/VoteChecker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.level2_c__methods_practices
+{
+    internal class StudentVoteChecker
+    {
+        public static void Main(string[] args)
+        {
+            int[] studentAges = new int[10];
+            for (int i = 0; i < studentAges.Length; i++)
+            {
+                studentAges[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            StudentVoteChecker check = new StudentVoteChecker();
+            for (int i = 0; i < studentAges.Length; i++)
+            {
+                bool checkedVote = check.StudentVoteChecked(studentAges[i]);
+                if (checkedVote)
+                {
+                    Console.WriteLine("The Student With Age" + " " + studentAges[i] + " " + "Can Vote");
+                }
+                else
+                {
+                    Console.WriteLine("The Student With Age" + " " + studentAges[i] + " " + "Cannot Vote");
+                }
+            }
+        }
+        public bool StudentVoteChecked(int voteToBeChecked)
+        {
+            if (voteToBeChecked < 0)
+            {
+                return false;
+            }
+            else if (voteToBeChecked >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
