891dd63a3760538f16adf3893cf405dd2e12e15d
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-25 19:23:38 +0530

    [Harsh Upadhyay].Add Practice Problems and Solution Of Sum of Natural Numbers Problem Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/c#-methods/level1-c#-method-practices/SumOfNaturalNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.level1_c__methods_practices
+{
+   internal class SumOfNaturalNumbers
+   {
+       public static void Main(string[] args)
+       {
+           //Getting integer input from the user for calculating the sum of n natural numbers 
+           int number = Convert.ToInt32(Console.ReadLine());
+           //Creating an instance or object of the class to access non static method in a static method
+           SumOfNaturalNumbers sN = new SumOfNaturalNumbers();
+           //Calling a method to calculate the sum of n natural numbers and storing it in a variable to print further
+           int sumOfNatural = sN.CalculateSumOfNaturalNumbers(number);
+           Console.Write("The Sum of" + " " + number + " " + "natural numbers is" + " " + sumOfNatural);
+       }
+       //Initailizing the variable sum to calculate the sum of n natural numbers
+       int sumCalculate = 0;
+       public int CalculateSumOfNaturalNumbers(int number)
+       {
+           //Using a for loop to calculate the sum of n natural numbers
+           for(int i = 0; i <= number; i++)
+           {
+               //Adding the value of i to the sum variable for calculating the sum of n natural numbers
+               sumCalculate += i;
+           }
+           //Returning the sum of the n natural numbers to method where it has been called
+           return sumCalculate;
+       }
+   }
+}
