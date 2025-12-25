b79e90c9b75b8464c857bce8e4735f783cd8552e
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-25 19:20:48 +0530

    [Harsh Upadhyay].Add Practice Problems and Solution Of Possible Hand Shakes Problem Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/c#-methods/level1-c#-method-practices/PossibleHandShakes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.level1_c__methods_practices
+{
+    internal class PossibleHandShakes
+    {
+        public static void Main(string[] args)
+        {
+            int numberOfStudents=Convert.ToInt32(Console.ReadLine());
+            double combinationOfPossibleHandShakes=(numberOfStudents*(numberOfStudents-1))/2;
+            Console.WriteLine("The Number of Possible Handshakes is"+" "+combinationOfPossibleHandShakes);
+        }
+    }
+}
