b0d0a1c5d225a27b98c346d9f9a5346e68b3c832
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-25 22:10:21 +0530

    [Harsh Upadhyay].Add Practice Problems and Solution of Otp Generator Problem Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/c#-methods/level3-c#-method-practices/otpGenerator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace BridgeLabzTraining.level3_c__methods_practices
+{
+    internal class OtpGenerator
+    {
+        public static void Main(string[] args)
+        {
+            // Creating an array to save the OTP numbers generated 10 times
+            int[] otpNumbers = new int[10];
+
+            // Generating 6-digit OTP numbers 10 times
+            for (int i = 0; i < otpNumbers.Length; i++)
+            {
+                otpNumbers[i] = GenerateSixDigitOtp();
+            }
+
+            // Displaying the generated OTP numbers
+            Console.WriteLine("Generated OTP Numbers:");
+            for (int i = 0; i < otpNumbers.Length; i++)
+            {
+                Console.WriteLine("OTP " + (i + 1) + " = " + otpNumbers[i]);
+            }
+
+            // Calling method to check whether all OTP numbers are unique
+            bool isUnique = AreOtpsUnique(otpNumbers);
+
+            // Displaying the result
+            if (isUnique)
+            {
+                Console.WriteLine("All generated OTP numbers are unique");
+            }
+            else
+            {
+                Console.WriteLine("Duplicate OTP numbers found");
+            }
+        }
+
+        // Method to Generate a 6-digit OTP number using Math.Random()
+        public static int GenerateSixDigitOtp()
+        {
+            // Generating a random 6-digit number between 100000 and 999999
+            int otp = (int)(Math.Random() * 900000) + 100000;
+            return otp;
+        }
+
+        // Method to ensure that the OTP numbers generated are unique
+        // If unique return true else return false
+        public static bool AreOtpsUnique(int[] otpNumbers)
+        {
+            // Comparing each OTP with every other OTP
+            for (int i = 0; i < otpNumbers.Length; i++)
+            {
+                for (int j = i + 1; j < otpNumbers.Length; j++)
+                {
+                    if (otpNumbers[i] == otpNumbers[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
