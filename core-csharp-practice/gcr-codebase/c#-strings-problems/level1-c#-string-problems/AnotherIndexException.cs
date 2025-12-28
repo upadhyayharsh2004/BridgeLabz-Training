c71a3ab76ce5a4a091491993b05c66fe2e1e9c81
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-28 22:52:03 +0530

    [Harsh Upadhyay].Add Practice Problems and Solution Of Another Index Exception Problem Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/c#-strings-problems/level1-c#-string-problems/AnotherIndexException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace BridgeLabzTraining.level1_c__string_problems
+{
+    internal class AnotherIndexException
+    {
+        public static void Main()
+        {
+            DemonstrateArrayIndexOutOfRangeException();
+        }
+        // Method to demonstrate IndexOutOfRangeException for arrays
+        static void DemonstrateArrayIndexOutOfRangeException()
+        {
+            int[] numbers = {10,20,30,40,50};
+            try
+            {
+                // Invalid index access
+                // Valid indexes:0 to 4
+                int value=numbers[7]; // Causes IndexOutOfRangeException
+                Console.WriteLine("Value:"+value);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("IndexOutOfRangeException caught!");
+                Console.WriteLine("Message:"+ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program execution continues...");
+            }
+        }
+    }
+}
