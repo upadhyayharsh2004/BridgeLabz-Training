bd02ccfc4ee0b9818271c7b77e1beef8f117ffbd
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-28 22:54:18 +0530

    [Harsh Upadhyay].Add Practice Problems and Solution Of Exception Handling Problem Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/c#-strings-problems/level1-c#-string-problems/ExceptionHandling.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace BridgeLabzTraining.level1_c__string_problems
+{
+    internal class ExceptionHandling
+    {
+        public static void Main()
+        {
+            DemonstrateNullReferenceException();
+        }
+        // Method to demonstrate NullReferenceException
+        static void DemonstrateNullReferenceException()
+        {
+            string message=null;//null string
+            try
+            {
+                // Attempt to access a method on a null object
+                int length=message.Length;//Causes NullReferenceException
+                Console.WriteLine("Length:"+length);
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("NullReferenceException caught!");
+                Console.WriteLine("Message:"+ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program execution continues...");
+            }
+        }
+    }
+}
