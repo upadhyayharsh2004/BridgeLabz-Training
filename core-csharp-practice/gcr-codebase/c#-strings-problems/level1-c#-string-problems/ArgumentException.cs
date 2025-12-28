f00d281b7387caf691d97ad0e322b16fd196cb3d
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-28 22:52:33 +0530

    [Harsh Upadhyay].Add Practice Problems and Solution Of Argument Exception Problem Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/c#-strings-problems/level1-c#-string-problems/ArgumentException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace BridgeLabzTraining.level1_c__string_problems
+{
+    internal class ArgumentException
+    {
+        public static void Main()
+        {
+            DemonstrateArgumentOutOfRangeException();
+        }
+        // Method to demonstrate ArgumentOutOfRangeException
+        static void DemonstrateArgumentOutOfRangeException()
+        {
+            string text="Programming";
+            try
+            {
+                // Invalid usage of Substring
+                // Start index = 8, length = 10 (goes beyond string length)
+                string result=text.Substring(8,10);
+                Console.WriteLine(result);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("ArgumentOutOfRangeException caught!");
+                Console.WriteLine("Message:"+ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program execution continues...");
+            }
+        }
+    }
+}
