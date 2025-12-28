b7a94ac311acb3edbd18c6108d166ba268873eaa
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-28 23:02:31 +0530

    [Harsh Upadhyay].Add Practice Problems and Solution Of Time Zones Problem Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/extras-c#-string-problems/date-time-c#-string-problems/TimeZonesProblems.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace BridgeLabzTraining.level1_c__string_problems
+{
+    internal class TimeZonesProblems
+    {
+        public static void Main()
+        {
+            // Get current UTC time
+            DateTimeOffset utcNow = DateTimeOffset.UtcNow;
+            // Time zone definitions
+            TimeZoneInfo gmtZone=TimeZoneInfo.Utc;
+            TimeZoneInfo istZone=TimeZoneInfo.FindSystemTimeZoneById("India Standard Time");
+            TimeZoneInfo pstZone=TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+            // Convert time to each time zone
+            DateTimeOffset gmtTime=TimeZoneInfo.ConvertTime(utcNow, gmtZone);
+            DateTimeOffset istTime=TimeZoneInfo.ConvertTime(utcNow, istZone);
+            DateTimeOffset pstTime=TimeZoneInfo.ConvertTime(utcNow, pstZone);
+            // Display output
+            Console.WriteLine("Current Time in Different Time Zones:");
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine("GMT:"+gmtTime);
+            Console.WriteLine("IST:"+istTime);
+            Console.WriteLine("PST:"+pstTime);
+        }
+    }
+}
