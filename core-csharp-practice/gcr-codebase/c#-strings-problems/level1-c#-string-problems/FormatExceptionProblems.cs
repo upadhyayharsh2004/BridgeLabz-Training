96b416e63c8bacceb27eddae109d25c1d9743000
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-28 22:54:40 +0530

    [Harsh Upadhyay].Add Practice Problems and Solution Of Format Exception Handling Problem Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/c#-strings-problems/level1-c#-string-problems/FormatExceptionProblems.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace BridgeLabzTraining.level1_c__string_problems
+{
+    internal class FormatExceptionProblems
+    {
+        public static void Main()
+        {
+            DemonstrateFormatException();
+        }
+        // Method to demonstrate FormatException
+        static void DemonstrateFormatException()
+        {
+            string value="abc";//Non-numeric string
+            try
+            {
+                // Attempt to convert non-numeric string to int
+                int number=int.Parse(value);
+                Console.WriteLine("Number:"+number);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("FormatException caught!");
+                Console.WriteLine("Message:"+ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Program execution continues...");
+            }
+        }
+    }
+}
