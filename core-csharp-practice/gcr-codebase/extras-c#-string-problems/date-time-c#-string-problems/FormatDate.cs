cad4458a393a7a20af644c402b59201cef491b9b
upadhyayharsh2004 <harshupa2004@gmail.com>
2025-12-28 23:02:03 +0530

    [Harsh Upadhyay].Add Practice Problems and Solution Of Format Date Problem Given By BridgeLabz

---
+++ core-csharp-practice/gcr-codebase/extras-c#-string-problems/date-time-c#-string-problems/FormatDate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace BridgeLabzTraining.level1_c__string_problems
+{
+    internal class FormatDate
+    {
+        public static void Main()
+        {
+            // Get current date
+            DateTime currentDate=DateTime.Now;
+            // Display date in different formats
+            Console.WriteLine("Current Date in Different Formats:\n");
+            Console.WriteLine("dd/MM/yyyy:"+currentDate.ToString("dd/MM/yyyy"));
+            Console.WriteLine("yyyy-MM-d:"+currentDate.ToString("yyyy-MM-dd"));
+            Console.WriteLine("EEE,MMM dd,yyyy:"+currentDate.ToString("ddd,MMM dd, yyyy"));
+        }
+    }
+}
