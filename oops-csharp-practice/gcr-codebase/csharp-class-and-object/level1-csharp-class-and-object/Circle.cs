ad6992fb1c2e576bd57c41c974929e6255d6c404
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-04 22:29:07 +0530

    [Harsh Upadhyay] Add  .Circle class with radius attribute, methods to calculate area and circumference, and demonstration of object usage and user input.

---
+++ oops-csharp-practice/gcr-codebase/csharp-class-and-object/level1-csharp-class-and-object/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.oops_csharp_practice.gcr_codebase.csharp_class_and_object.level_1
+{
+    internal class Circle
+    {
+        //attribute to store data
+        double radius;
+
+        public static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            Console.Write("ENTER RADIUS: ");
+            circle.radius = Convert.ToDouble(Console.ReadLine());
+            circle.CalculateArea();
+            circle.CalculateCircumference();
+
+        }
+        public void CalculateArea()
+        {
+            double area = Math.PI * radius * radius;
+            Console.WriteLine("Area of circle: " + area);
+        }
+        public void CalculateCircumference()
+        {
+
+            double circumference = 2 * Math.PI * radius;
+            Console.WriteLine("Circumference of circle: " + circumference);
+        }
+    }
+}
