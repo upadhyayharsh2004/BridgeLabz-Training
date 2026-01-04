f5f220eff67331741e0ee54af19831648a682d34
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-04 23:01:06 +0530

    [Harsh Upadhyay] Add .Person class demonstrating parameterized and copy constructors with object creation and display of personal details.

---
+++ oops-csharp-practice/gcr-codebase/csharp-constructors/level1-csharp-constructors/Person.cs
@@ -0,0 +1,66 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.oops_csharp_practice.gcr_codebase.csharp_constructors.level_1
+{
+    internal class Person
+    {
+        //attributes to store data
+
+        string name;
+
+        int age;
+
+        //create parameterized constructor
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        //create copy constructor
+        public Person(Person person)
+        {
+            this.name = person.name;
+            this.age = person.age;
+        }
+
+        public static void Main(string[] args)
+        {
+            //take name input
+            Console.Write("ENTER NAME: ");
+            string name = Console.ReadLine();
+
+            //take age input
+            Console.Write("ENTER AGE: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            //create object using parameterized constructor
+            Person Person1 = new Person(name, age);
+
+            //create objecy using copy constructor
+            Person Person2 = new Person(Person1);
+
+            //call DisplayDetails Method
+            Console.WriteLine("\nUSING PARAMETERIZED CONSTRUCTOR:");
+            Person1.DisplayDetails();
+
+            Console.WriteLine("\nUSING COPY CONSTRUCTOR:");
+            Person2.DisplayDetails();
+
+        }
+        public void DisplayDetails()
+        {
+            //display details of person
+            Console.WriteLine("Person Name: " + name);
+
+            Console.WriteLine("Person Age: " + age);
+
+        }
+
+    }
+}
