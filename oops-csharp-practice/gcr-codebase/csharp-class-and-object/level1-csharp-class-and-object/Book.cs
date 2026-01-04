14af87ab4add1d109609bc4831db2a6cb749976f
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-04 22:27:45 +0530

    [Harsh Upadhyay] Add  .Book class with attributes title, author, and price, demonstrating object creation, user input, and display of book details.

---
+++ oops-csharp-practice/gcr-codebase/csharp-class-and-object/level1-csharp-class-and-object/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.oops_csharp_practice.gcr_codebase.csharp_class_and_object.level_1
+{
+    internal class Book
+    {
+
+        string title;
+
+        string author;
+
+        double price;
+
+        public static void Main(string[] args)
+        {
+            Book book = new Book();
+            Console.Write("ENTER BOOK TITLE: ");
+            book.title = Console.ReadLine();
+            Console.Write("ENTER AUTHOR NAME: ");
+            book.author = Console.ReadLine();
+            Console.Write("ENTER BOOK PRICE: ");
+            book.price = Convert.ToDouble(Console.ReadLine());
+            book.DisplayDetails();
+
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine("Book Title: " + title);
+
+            Console.WriteLine("Author Name: " + author);
+
+            Console.WriteLine("Book Price: " + price);
+        }
+    }
+}
