e4a2a11c376d5542e83f4f9f946460409083cc5b
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-04 22:58:14 +0530

    [Harsh Upadhyay] Add .Circle class demonstrating default and parameterized constructors with method to calculate area.

---
+++ oops-csharp-practice/gcr-codebase/csharp-constructors/level1-csharp-constructors/Circle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.oops_csharp_practice.gcr_codebase.csharp_constructors.level_1
+{
+    internal class Circle
+    {
+        //attributes to store data
+
+        double radius;
+
+        //create default constructor
+        public Circle() : this(10)
+        {
+
+        }
+
+        //create parameterized constructor
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public static void Main(string[] args)
+        {
+            //create object using default constructor
+            Circle circle1 = new Circle();
+
+            //take radius input
+            Console.Write("ENTER RADIUS: ");
+            double radius = Convert.ToDouble(Console.ReadLine());
+
+            //create object using parameterized constructor
+            Circle circle2 = new Circle(radius);
+
+            //call CalculateArea Method
+            circle1.CalculateArea();
+
+            circle2.CalculateArea();
+
+        }
+        public void CalculateArea()
+        {
+            //calculate area of circle
+            double area = Math.PI * radius * radius;
+
+            Console.WriteLine("AREA OF CIRCLE: " + area);
+        }
+
+    }
+}
