3a77c9144e49fc5bbf957d513131717d5fdd8cfa
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-04 22:20:38 +0530

    [Harsh Upadhyay] Add .Book and Ebook classes demonstrating public, protected, and private access modifiers with getter/setter for author and input/display functionality.

---
+++ oops-csharp-practice/gcr-codebase/csharp-access-modifiers/Book.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.oops_csharp_practice.gcr_codebase.csharp_access_modifiers
+{
+    internal class Book
+    {
+        public string isbn;
+        protected string title;
+        private string author;
+        public void setAuthor(string author)
+        {
+            this.author = author;
+        }
+        public string getAuthor()
+        {
+            return this.author;
+        }
+    }
+    internal class Ebook : Book
+    {
+        public static void Main(string[] args)
+        { 
+            TakeInput();
+        }
+
+        public static void TakeInput()
+        {
+            //create object
+            Ebook ebook = new Ebook();
+
+            //take isbn input
+            Console.WriteLine("ENTER ISBN: ");
+            ebook.isbn = Console.ReadLine();
+
+            //take title input
+            Console.WriteLine("ENTER TITLE: ");
+            ebook.title = Console.ReadLine();
+
+            //take author input
+            Console.WriteLine("ENTER AUTHOR: ");
+            ebook.setAuthor(Console.ReadLine());
+
+            ebook.DisplayDetails();
+        }
+        public void DisplayDetails()
+        {
+            Console.WriteLine("ISBN: " + isbn);
+
+            Console.WriteLine("TITLE: " + title);
+
+            Console.WriteLine("AUTHOR: " + getAuthor());
+        }
+    }
+}
