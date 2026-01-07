62bea005bc088d4ac8890af29f2f7c5b4a70757d
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-07 22:45:07 +0530

    [Harsh Upadhyay] Add .Problems Of Food Delivery Management System Using OOPS Concept Given By BridgeLabz

---
+++ oops-csharp-practice/gcr-codebase/csharp-oops-practice/FoodDeliverySystem.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+
+namespace Bridgelabz.Training_oops_scenario_based
+{
+    internal interface IDiscountable
+    {
+        double ApplyDiscount();
+        void GetDiscountDetails();
+    }
+    abstract class FoodItem
+    {
+        public string itemName;
+        public double price;
+        public int quantity;
+        public FoodItem(string name, double price, int quantity)
+        {
+            this.itemName = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+        public abstract double CalculateTotalPrice();
+        public abstract double ApplyDiscount();
+        public abstract void GetDiscountDetails();
+        public void GetItemDetails()
+        {
+            Console.WriteLine("Item name : " + itemName);
+            Console.WriteLine("Price : " + price);
+            Console.WriteLine("Quantity : " + quantity);
+        }
+    }
+    class VegItem : FoodItem, IDiscountable
+    {
+        public VegItem(string name, double price, int quantity) : base(name, price, quantity) { }
+        public override double CalculateTotalPrice()
+        {
+            return price * quantity;
+        }
+        public override double ApplyDiscount()
+        {
+            return CalculateTotalPrice() * 0.10;
+        }
+        public override void GetDiscountDetails()
+        {
+            Console.WriteLine("Veg Item Discount: 10%");
+        }
+    }
+    class NonVegItem : FoodItem, IDiscountable
+    {
+        public NonVegItem(string name, double price, int quantity) : base(name, price, quantity) { }
+        public override double CalculateTotalPrice()
+        {
+            return (price * quantity) + 50;
+        }
+        public override double ApplyDiscount()
+        {
+            return CalculateTotalPrice() * 0.05;
+        }
+        public override void GetDiscountDetails()
+        {
+            Console.WriteLine("Non-Veg Item Discount: 5%");
+        }
+    }
+    class FoodDeliverySystem
+    {
+        static void Main()
+        {
+            FoodItem item1 = new VegItem("Paneer Burger", 120, 2);
+            FoodItem item2 = new NonVegItem("Chicken Burger", 250, 1);
+            DisplayItem(item1);
+            Console.WriteLine();
+            DisplayItem(item2);
+        }
+        static void DisplayItem(FoodItem item)
+        {
+            item.GetItemDetails();
+            Console.WriteLine("Total Price : " + item.CalculateTotalPrice());
+            item.GetDiscountDetails();
+            Console.WriteLine("Discount Amount : " + item.ApplyDiscount());
+        }
+    }
+}
