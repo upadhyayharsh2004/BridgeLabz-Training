409e72bb1bf4b3ae2f9bfd1c55f6b19dd89c91c9
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-07 16:50:58 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Employee Wages Problems Given By BridgeLabz in The Scenario Based Folder

---
+++ oops-csharp-practice/scenario-based/c#-employee-wages-problems/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+namespace Bridgelabz.Training_oops_scenario_based_Employee_Wages_Problems
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int TotalWage { get; private set; }
+        public int TotalHours { get; private set; }
+        public int TotalDays { get; private set; }
+        public Employee(int empId, string empName)
+        {
+            EmpId = empId;
+            EmpName = empName;
+            TotalWage = 0;
+            TotalHours = 0;
+            TotalDays = 0;
+        }
+        public void AddHours(int hours)
+        {
+            TotalHours += hours;
+        }
+        public void AddWage(int wage)
+        {
+            TotalWage += wage;
+        }
+        public void AddDay()
+        {
+            TotalDays++;
+        }
+    }
+}
---
+++ oops-csharp-practice/scenario-based/c#-employee-wages-problems/EmployeeMain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridgelabz.Training_oops_scenario_based_Employee_Wages_Problems
+{
+    public class EmployeeMain
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Employee employee = new Employee(101, "John");
+            IEmployee service = new EmployeeUtilityImpl();
+            EmployeeMenu.ShowMenu(service);
+            Console.ReadLine();
+        }
+    }
+
+}
