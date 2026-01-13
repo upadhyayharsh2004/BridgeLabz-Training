2291326b9878bb3f39685a70e5b5e6695592d89f
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-13 22:55:14 +0530

    [Harsh Upadhyay] Add .Practice Problems Of First Negative Problem In The Topic Of Searching Given By BridgeLabz

---
+++ dsa-csharp-practice/gcr-codebase/csharp-linear-binary-search/PeakElement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.dsa_csharp_practice.gcr_codebase.csharp_linear_binary_search
+{
+    internal class PeakElement
+    {
+        public static void Main(string[] args)
+        {
+            //take array size
+            Console.Write("ENTER SIZE OF ARRAY: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            //create array to store numbers
+            int[] numbers = new int[size];
+
+            //loop to take number inputs
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("ENTER NUMBER: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            //call FindFirstNegative method
+            FindPeakElement(numbers);
+        }
+
+
+        //method to find rotated point 
+        public static void FindPeakElement(int[] numbers)
+        {
+            //initialize low and high index
+            int low = 0;
+            int high = numbers.Length - 1;
+
+            //loop until low is less than high
+            while (low < high)
+            {
+                //find mid 
+                int mid = (low + high) / 2;
+
+                if (numbers[mid] < numbers[mid + 1])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid;
+                }
+            }
+
+            Console.WriteLine("PEAK ELEMENT AT INDEX: " + low);
+        }
+    }
+}
