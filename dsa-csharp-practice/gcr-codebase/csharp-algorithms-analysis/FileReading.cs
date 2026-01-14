8a2152dff3e6b15c0ffcbc9ce1fa28c325c998dd
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-14 21:19:29 +0530

    [Harsh Upadhyay] Add .Practice Problems Of File Reading Problem In The Topic Of Algorithm Given By BridgeLabz

---
+++ dsa-csharp-practice/gcr-codebase/csharp-algorithms-analysis/FileReading.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.dsa_csharp_practice.gcr_codebase.csharp_algorithms_analysis
+{
+    internal class FileReading
+    {
+        public static void Main(string[] args)
+        {
+
+            //start time for stream reader
+            DateTime srStart = DateTime.Now;
+
+            StreamReader reader = new StreamReader("sample.txt");
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                //reading line by line
+            }
+
+            //end time for stream reader
+            DateTime srEnd = DateTime.Now;
+
+            Console.WriteLine("StreamReader TIME: " + (srEnd - srStart).TotalMilliseconds + " ms");
+
+            reader.Close();
+            Console.WriteLine("FILE READ USING StreamReader");
+
+
+            //start time for file stream
+            DateTime fsStart = DateTime.Now;
+
+            FileStream fs = new FileStream("sample.txt", FileMode.Open);
+            byte[] buffer = new byte[1024];
+
+            while (fs.Read(buffer, 0, buffer.Length) > 0)
+            {
+                //reading raw bytes
+            }
+
+            //end time for file stream
+            DateTime fsEnd = DateTime.Now;
+            Console.WriteLine("FileStream TIME: " + (fsEnd - fsStart).TotalMilliseconds + " ms");
+
+            fs.Close();
+            Console.WriteLine("FILE READ USING FileStream");
+        }
+    }
+}
