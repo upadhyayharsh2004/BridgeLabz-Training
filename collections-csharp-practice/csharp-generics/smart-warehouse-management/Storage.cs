189df2b5ed39c5c5b3865549bd72c8bb9890c409
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-20 22:23:03 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Generic Smart Warehouse Management in The Topic Of Generics Given By BridgeLabz

---
+++ collections-csharp-practice/csharp-generics/smart-warehouse-management/Storage.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.collections_csharp_practice.csharp_generics.smart_warehouse_management
+{
+    internal class Storage<T> where T : WareHouseItem 
+    {
+        //array to store items
+        T[]items=new T[10];
+
+        //variable to store total items count
+        int totalItems = 0;
+
+        //constructor
+        public Storage(int size)
+        {
+            items = new T[size];
+        }
+
+        //method to add item 
+        public void AddItem(T item)
+        {
+            if (totalItems < items.Length)
+            {
+                items[totalItems++] = item;
+            }
+            else
+            {
+                Console.WriteLine("STORAGE FULL");
+            }
+        }
+
+        //method to display all items
+        public void DisplayItems()
+        {
+           for(int i = 0; i < totalItems; i++)
+            {
+                items[i].DisplayInfo();
+            }
+        }
+    }
+}
