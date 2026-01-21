2e4002c7dc8aee21d23786f4ef1cc34db61bc86d
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-21 22:22:36 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Collections Convert Set To Sorted List in The Topic Of Collections Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-collections/ConvertSetToSortedList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_collections
+{
+    internal class ConvertSetToSortedList
+    {
+        public static void Main(string[] args)
+        {
+            //take number of elements
+            Console.WriteLine("Enter Number of Elements: ");
+            int numberOfElements = Convert.ToInt32(Console.ReadLine());
+
+            //hashset to store numbers
+            HashSet<int> numbers = new HashSet<int>();
+
+            //tale elements
+            Console.WriteLine("Enter Elements:");
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                Console.Write("Enter Element: ");
+                numbers.Add(Convert.ToInt32(Console.ReadLine()));
+            }
+
+            //call ConvertToSortedListmethod
+            ConvertToSortedList(numbers);
+        }
+
+        //method to convert set to sorted list
+        public static void ConvertToSortedList(HashSet<int> numbers)
+        {
+            //sorted list
+            List<int> sortedList = new List<int>(numbers);
+            sortedList.Sort();
+
+            //display result    
+            Console.WriteLine("Sorted List:");
+            foreach (int num in sortedList)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
