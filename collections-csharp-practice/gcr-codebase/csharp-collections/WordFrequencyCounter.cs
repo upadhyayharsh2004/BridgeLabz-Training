7bbd61a3f44ce6fc3a92537488abe0ceca95dbd0
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-21 22:39:29 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Collections of Word Frequency Counter in The Topic Of Collections Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-collections/WordFrequencyCounter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_collections
+{
+    internal class WordFrequencyCounter
+    {
+        public static void Main(string[] args)
+        {
+            //take input sentence
+            Console.WriteLine("Enter Text:");
+            string text = Console.ReadLine();
+
+            //call CountWords method
+            CountWords(text);
+        }
+
+
+        //method to count word frequency
+        public static void CountWords(string text)
+        {
+            //dictionary to store word count
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+
+            //convert to lowercase and split words
+            string[] words = text.ToLower().Split(' ', ',', '!', '.');
+
+            //loop through words
+            foreach (string word in words)
+            {
+
+                if (word == "")
+                    continue;
+
+                if (wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+                else
+                {
+                    wordCount.Add(word, 1);
+                }
+            }
+
+            Console.WriteLine("Word Frequency:");
+            foreach (var item in wordCount)
+            {
+                Console.WriteLine(item.Key + " : " + item.Value);
+            }
+        }
+    }
+}
