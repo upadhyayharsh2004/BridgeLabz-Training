148d33d2732c2026ad3cc207ee7798a31a7832d3
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-21 22:34:16 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Collections of Reverse A Elements in The Topic Of Collections Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-collections/RotateElements.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_collections
+{
+    internal class RotateElements
+    {
+        public static void Main(string[] args)
+        {
+            //take number of elements
+            Console.Write("Enter Number Of Elements: ");
+            int numberOfElements = Convert.ToInt32(Console.ReadLine());
+
+            //list to store numbers
+            List<int> numbers = new List<int>();
+
+            //loop to take number inputs
+            for (int i = 0; i < numberOfElements; i++)
+            {
+                //take number
+                Console.Write("Enter Number: ");
+                int value = Convert.ToInt32(Console.ReadLine());
+                numbers.Add(value);
+            }
+
+            //take rotate count
+            Console.Write("Enter Rotate Count: ");
+            int rotateBy = Convert.ToInt32(Console.ReadLine());
+
+            //call RotateList method
+            RotateList(numbers, rotateBy);
+        }
+
+        //method to rotate list
+        public static void RotateList(List<int> numbers, int rotateBy)
+        {
+            //store rotated list
+            List<int> rotatedList = new List<int>();
+
+            //add elements from rotate position
+            for (int i = rotateBy; i < numbers.Count; i++)
+            {
+                rotatedList.Add(numbers[i]);
+            }
+
+            //add first elements at end
+            for (int i = 0; i < rotateBy; i++)
+            {
+                rotatedList.Add(numbers[i]);
+            }
+
+            //display rotated list
+            Console.WriteLine("Rotated List:");
+            foreach (int num in rotatedList)
+            {
+                Console.Write(num + " ");
+            }
+        }
+    }
+}
