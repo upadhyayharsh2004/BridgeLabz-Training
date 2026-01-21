1db0d8d8af9b1df4248964078567170a901b8d9e
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-21 22:29:25 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Collections of Hospital Triage System in The Topic Of Collections Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-collections/HospitalTriageSystem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_collections
+{
+    internal class HospitalTriageSystem
+    {
+        public static void Main(string[] args)
+        {
+            //priority queue
+            PriorityQueue<string, int> patients = new PriorityQueue<string, int>();
+
+            //add patients with severity (negative for max priority)
+            patients.Enqueue("Yash", -3);
+            patients.Enqueue("Prashant", -5);
+            patients.Enqueue("Varun", -2);
+
+            //call Treatment method
+            Treatment(patients);
+        }
+
+        //method to treat patients based on priority
+        public static void Treatment(PriorityQueue<string, int> patients)
+        {
+            Console.WriteLine("Treatment Order:");
+
+            while (patients.Count > 0)
+            {
+                Console.WriteLine(patients.Dequeue());
+            }
+        }
+    }
+}
