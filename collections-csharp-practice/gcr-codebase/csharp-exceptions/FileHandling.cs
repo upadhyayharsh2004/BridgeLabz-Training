68b47b326e9fe56f47f5d9092c5ab63b8c3166e8
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-23 21:48:28 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Exceptions of File Handling Example in The Topic Of Exceptions Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-exceptions/FileHandling.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_exceptions
+{
+    internal class FileHandling
+    {
+        public static void Main(string[] args)
+        {
+            //try block
+            try
+            {
+                //using statement ensures StreamReader is closed automatically
+                using (StreamReader reader = new StreamReader("info.txt"))
+                {
+                    //read the first line of the file
+                    string line = reader.ReadLine();
+                    Console.WriteLine("First line: " + line);
+                }
+            }
+            //catch IOException
+            catch (IOException)
+            {
+                Console.WriteLine("IOException occurred: Error reading file");
+            }
+        }
+    }
+}
