a830ec343bb488420c8d35695215149459a6c5bb
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-23 21:50:05 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Exceptions of Mutiple Exceptions Example in The Topic Of Exceptions Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-exceptions/MultipleExceptions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_exceptions
+{
+    internal class MultipleExceptions
+    {
+        public static void Main(string[] args)
+        {
+            //initialize array with null
+            int[] numbers = null;
+
+            //try block
+            try
+            {
+                //take user input for index
+                Console.Write("ENTER INDEX: ");
+                int index = int.Parse(Console.ReadLine());
+
+                //get value at given index
+                int value = numbers[index];
+
+                //if no exception occurred
+                Console.WriteLine("Value at index " + index + ": " + value);
+            }
+            //catch IndexOutOfRangeException
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("IndexOutOfRangeException occurred: Invalid index!");
+            }
+            //catch NullReferenceException
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("NullReferenceException occurred: Array is not initialized!");
+            }
+            //catch FormatException
+            catch (FormatException)
+            {
+                Console.WriteLine("FormatException occurred: Please enter a valid numeric value");
+            }
+        }
+    }
+}
