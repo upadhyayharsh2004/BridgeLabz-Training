5029b490499e4be24e67c81055b2179849d7fab8
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-23 21:48:52 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Exceptions of File Not Found Example in The Topic Of Exceptions Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-exceptions/FileNotFoundExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_exceptions
+{
+    internal class FileNotFoundExample
+    {
+        public static void Main(string[] args)
+        {
+            //try block
+            try
+            {
+                //read all text from the file
+                string content = File.ReadAllText("data.txt");
+                Console.WriteLine(content);
+            }
+            //catch FileNotFoundException
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("FileNotFoundException occurred");
+            }
+            //catch IOException
+            catch (IOException ex)
+            {
+                Console.WriteLine("IOException occurred: " + ex.Message);
+            }
+        }
+    }
+}
