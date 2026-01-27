b53231b7017cf45b6ba7fb19b200fdeda278a18a
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-27 21:34:16 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Replace And Modify Regex Example in The Topic Of Regex Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-regex-nunit/csharp-regex/ReplaceAndModify.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+//using System.Threading.Tasks;
+
+//namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_regex_nunit.csharp_regex
+//{
+//    internal class ReplaceAndModify
+//    {
+//        public static void Main(string[] args)
+//        {
+//            //text to store sentence with spaces
+//            string text = "This   is   an   example";
+
+//            //regex pattern
+//            string pattern = @"\s+";
+
+//            //replace with single space
+//            string result = Regex.Replace(text, pattern, " ");
+
+//            //display result
+//            Console.WriteLine(result);
+//        }
+//    }
+//}
