328d290de7126b8a7ae17db8b5bc5706080e4fd2
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-27 21:33:12 +0530

    [Harsh Upadhyay] Add .Practice Problems Of License Plate Regex Example in The Topic Of Regex Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-regex-nunit/csharp-regex/LicensePlate.cs
@@ -0,0 +1,28 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+//using System.Threading.Tasks;
+
+//namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_regex_nunit.csharp_regex
+//{
+//    internal class LicensePlate
+//    {
+//        public static void Main(string[] args)
+//        {
+//            //license plate
+//            string plate = "TB8043";   
+
+//            //regex pattern
+//            string pattern = @"^[A-Z]{2}[0-9]{4}$";
+
+//            //check if pattern matches
+//            bool isValid = Regex.IsMatch(plate, pattern);
+
+//            //display result
+//            Console.WriteLine(isValid ? "Valid" : "Invalid");
+//        }
+//    }
+
+//}
