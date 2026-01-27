25725e21cd6bae146c6376ef90bde7e37c1094e8
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-27 21:35:33 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Validate Color Code Regex Example in The Topic Of Regex Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-regex-nunit/csharp-regex/ValidateColorCode.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+//using System.Threading.Tasks;
+
+//namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_regex_nunit.csharp_regex
+//{
+//    internal class ValidateColorCode
+//    {
+//        public static void Main(string[] args)
+//        {
+//            //hex color code
+//            string color = "#FFA500";   
+
+//            //regex pattern
+//            string pattern = @"^#[0-9A-Fa-f]{6}$";
+
+//            //validate pattern
+//            bool isValid = Regex.IsMatch(color, pattern);
+
+//            //display result
+//            Console.WriteLine(isValid ? "Valid" : "Invalid");
+//        }
+//    }
+//}
