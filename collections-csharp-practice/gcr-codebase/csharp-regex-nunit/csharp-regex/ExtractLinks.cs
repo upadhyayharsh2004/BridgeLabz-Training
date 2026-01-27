3e40949ae835f5c65a0d25bd20585e5f28116172
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-27 21:31:57 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Extract Links Regex Example in The Topic Of Regex Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-regex-nunit/csharp-regex/ExtractLinks.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+//using System.Threading.Tasks;
+
+//namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_regex_nunit.csharp_regex
+//{
+//    internal class ExtractLinks
+//    {
+//        public static void Main(string[] args)
+//        {
+//            //text to store links
+//            string text = "https://www.google.com , http://abcd.org, https://gla.ac.in";
+
+//            //regex pattern
+//            string pattern = @"https?://[^\s]+";
+
+//            //find all matches
+//            MatchCollection matches = Regex.Matches(text, pattern);
+
+//            //display results
+//            foreach (Match m in matches)
+//            {
+//                Console.WriteLine(m.Value);
+//            }
+//        }
+//    }
+//}
