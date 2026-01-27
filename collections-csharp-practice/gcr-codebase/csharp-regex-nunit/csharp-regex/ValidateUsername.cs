3050094f416c488177b0a7ce3db594902cc55f89
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-27 21:37:42 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Validate UserName Regex Example in The Topic Of Regex Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-regex-nunit/csharp-regex/ValidateUsername.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+//using System.Threading.Tasks;
+
+//namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_regex_nunit.csharp_regex
+//{
+//    internal class ValidateUsername
+//    {
+//        public static void Main(string[] args)
+//        {
+//            //username
+//            string username = "yash_2005";   
+
+//            //regex pattern
+//            string pattern = @"^[A-Za-z][A-Za-z0-9_]{4,14}$";
+
+//            //check if username matches pattern
+//            bool isValid = Regex.IsMatch(username, pattern);
+
+//            //display result
+//            Console.WriteLine(isValid ? "Valid" : "Invalid");
+//        }
+//    }
+//}
