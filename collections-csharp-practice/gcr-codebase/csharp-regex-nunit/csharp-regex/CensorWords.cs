d6363327be3afa7f106f08b687c0d41f0ae8fdce
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-27 21:25:11 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Censor Words Regex Example in The Topic Of Regex Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-regex-nunit/csharp-regex/CensorWords.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+//using System.Threading.Tasks;
+
+//namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_regex_nunit.csharp_regex
+//{
+//    internal class CensorWords
+//    {
+//        public static void Main(string[] args)
+//        {
+//            //store sentence
+//            string text = "This is a damn bad example with some stupid words.";
+
+//            //regex pattern with bad words list
+//            string pattern = @"damn|stupid";
+
+//            //replace bad words with ****
+//            string result = Regex.Replace(text, pattern, "****", RegexOptions.IgnoreCase);
+
+//            //dispkay results
+//            Console.WriteLine(result);
+//        }
+//    }
+//}
