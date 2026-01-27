869e21f8f2d508c6c14c97bf9debf81a54088acb
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-27 21:25:47 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Extract Capitalized Words Regex Example in The Topic Of Regex Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-regex-nunit/csharp-regex/ExtractCapitalizedWords.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+//using System.Threading.Tasks;
+
+//namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_regex_nunit.csharp_regex
+//{
+//    internal class ExtractCapitalizedWords
+//    {
+//        public static void Main(string[] args)
+//        {
+//            //text to store all words
+//            string text = "Hello World this is A Sentence example.";
+
+//            //regex pattern
+//            string pattern = @"\b[A-Z][a-z]*\b";
+
+//            //find all matches
+//            MatchCollection matches = Regex.Matches(text, pattern);
+
+//            //display result
+//            foreach (Match m in matches)
+//            {
+//                Console.WriteLine(m.Value);
+//            }
+//        }
+//    }
+//}
