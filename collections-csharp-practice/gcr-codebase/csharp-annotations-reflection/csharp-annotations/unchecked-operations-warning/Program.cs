d355661787141524eab355bea2da9aa2d46ed069
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-27 22:14:43 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Unchecked Operations Warning Attribute Method Example in The Topic Of Annonations And Reflection Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-annotations-reflection/csharp-annotations/unchecked-operations-warning/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_annotations_reflection.csharp_annotations.unchecked_operations_warning
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //disable warning for non-generic collections
+            #pragma warning disable 618
+
+            //create ArrayList without generics
+            ArrayList list = new ArrayList();
+
+            //add different data types
+            list.Add(10);
+            list.Add("Hello");
+
+            //enable warnings again
+            #pragma warning restore 618
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
