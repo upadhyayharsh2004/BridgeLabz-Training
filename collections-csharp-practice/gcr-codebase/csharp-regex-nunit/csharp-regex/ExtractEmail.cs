ebcba6e2a161e3d7f01eff9a5df4309af421e66f
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-27 21:31:30 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Extract Email Regex Example in The Topic Of Regex Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-regex-nunit/csharp-regex/ExtractEmail.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+//using System.Threading.Tasks;
+
+//namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_regex_nunit.csharp_regex
+//{
+//    internal class ExtractEmail
+//    {
+//        public static void Main(string[] args)
+//        {
+//            //store text with all emails
+//            string text = "yashkumar@gmail.com and support@gmail.com and abcdefg@gmail.com";
+
+//            //regex pattern
+//            string pattern = @"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}";
+
+//            //find all matches
+//            MatchCollection matches = Regex.Matches(text, pattern);
+
+//            //loop through matches
+//            foreach (Match m in matches)
+//            {
+//                Console.WriteLine(m.Value);
+//            }
+//        }
+//    }
+//}
