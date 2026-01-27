4a84845bb121e3edaa29368401752d477d036e8e
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-27 21:33:41 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Programming Language Name Regex Example in The Topic Of Regex Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-regex-nunit/csharp-regex/ProgrammingLanguageName.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+//using System.Threading.Tasks;
+
+//namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_regex_nunit.csharp_regex
+//{
+//    internal class ProgrammingLanguageName
+//    {
+//        public static void Main(string[] args)
+//        {
+//            //store sentence
+//            string text = "I love Java, Python, and JavaScript, but Go is cool.";
+
+//            //regex pattern
+//            string pattern = @"JavaScript|Java|Python|Go";
+
+//            //find all matches
+//            MatchCollection matches = Regex.Matches(text, pattern);
+
+//            //display results
+//            foreach (Match m in matches)
+//            {
+//                Console.WriteLine(m.Value);
+//            }
+//        }
+//    }
+//}
