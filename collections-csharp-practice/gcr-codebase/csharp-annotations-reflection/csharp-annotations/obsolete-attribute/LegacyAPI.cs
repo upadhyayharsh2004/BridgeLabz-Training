d96dd420d49068d3bec7e0b89cb374676f04039d
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-27 22:12:28 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Obsolete Attribute Method Example in The Topic Of Annonations And Reflection Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-annotations-reflection/csharp-annotations/obsolete-attribute/LegacyAPI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_annotations_reflection.csharp_annotations.obsolete_attribute
+{
+    internal class LegacyAPI
+    {
+        //old method 
+        [Obsolete("OldFeature is obsolete.")]
+        public void OldFeature()
+        {
+            Console.WriteLine("Old feature");
+        }
+
+        //new method
+        public void NewFeature()
+        {
+            Console.WriteLine("New feature");
+        }
+    }
+}
---
+++ collections-csharp-practice/gcr-codebase/csharp-annotations-reflection/csharp-annotations/obsolete-attribute/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_annotations_reflection.csharp_annotations.obsolete_attribute
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //create object of LegacyAPI
+            LegacyAPI api = new LegacyAPI();
+
+            //calling old method
+            api.OldFeature();
+
+            //calling new method
+            api.NewFeature();
+        }
+    }
+}
