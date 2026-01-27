3961459c2447cdb1a9acc904774adf1b4e625509
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-27 21:26:23 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Extract Currency Values Regex Example in The Topic Of Regex Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-regex-nunit/csharp-regex/ExtractCurrencyValues.cs
@@ -0,0 +1,30 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+//using System.Threading.Tasks;
+
+//namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_regex_nunit.csharp_regex
+//{
+//    internal class ExtractCurrencyValues
+//    {
+//        public static void Main(string[] args)
+//        {
+//            //store sentence with currency 
+//            string text = "The prices are $45.99,$ 10.50 and $50.";
+
+//            //regex pattern
+//            string pattern = @"\$?\s?\d+\.\d{2}";
+
+//            //find all matches
+//            MatchCollection matches = Regex.Matches(text, pattern);
+
+//            //display results
+//            foreach (Match m in matches)
+//            {
+//                Console.WriteLine(m.Value.Trim());
+//            }
+//        }
+//    }
+//}
