80c9af4e1ba4ac4688cbe2b58523b7768713cf31
upadhyayharsh2004 <harshupa2004@gmail.com>
2026-01-27 21:36:02 +0530

    [Harsh Upadhyay] Add .Practice Problems Of Validate Credit Card Regex Example in The Topic Of Regex Given By BridgeLabz

---
+++ collections-csharp-practice/gcr-codebase/csharp-regex-nunit/csharp-regex/ValidateCreditCard.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Text.RegularExpressions;
+//using System.Threading.Tasks;
+
+//namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_regex_nunit.csharp_regex
+//{
+//    internal class ValidateCreditCard
+//    {
+//        public static void Main(string[] args)
+//        {
+//            //store card 
+//            string card = "4111111111111111"; 
+
+//            //regex pattern
+//            string pattern = @"^(4\d{15}|5\d{15})$";
+
+//            //check if pattern matches
+//            bool isValid = Regex.IsMatch(card, pattern);
+
+//            //display result
+//            Console.WriteLine(isValid ? "Valid Card" : "Invalid Card");
+//        }
+//    }
+//}
